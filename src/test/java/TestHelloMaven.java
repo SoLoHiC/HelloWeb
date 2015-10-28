363b22a84f08bcfc723ab9271f2de9c612c93f8d
chuck <zimu.zhang@duitang.com>
2015-10-28 17:40:10 +0800

    init

---
+++ src/test/java/TestHelloMaven.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author chuck
+ * @since 10/28/15
+ */
+public class TestHelloMaven {
+
+  @Test
+  public void helloMavenTest() {
+    System.out.println("Hello Maven!");
+  }
+}

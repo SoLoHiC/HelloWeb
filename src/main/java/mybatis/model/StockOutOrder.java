732c0c92a6fab560f51ac89d70bb824e3fd910dc
chuck <neoz.o0312@gmail.com>
2017-01-17 10:19:40 +0800

    demo: sub select

--- src/main/java/mybatis/model/StockOutOrder.java
+++ src/main/java/mybatis/model/StockOutOrder.java
@@ -1,6 +1,9 @@
 package mybatis.model;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import lombok.Data;
 
 /**
@@ -14,4 +17,29 @@ public class StockOutOrder implements Serializable {
 
   private int id;
   private String bundleId;
+//  private Warehouse whId;
+//  private StockOutOrderType orderType;
+//  private StructureType structureType;
+  private long operatorId;
+//  private StockOutOrderStatus status;
+//  private SrcOrderType srcOrderType;
+  private String srcOrderId;
+  private int skuId;
+  private int quantity;
+  private String outerItemName;  // 由运营填写的商品名, 和商品库商品不一定对应
+  private String receiverName;
+  private String receiverTel;
+  private String receiverIdCard;
+  private String province;
+  private String city;
+  private String district;
+  private String road;
+  private Date gmtPush;
+  private String logisticsName;
+  private String logisticsTicket;
+  private Date gmtSend;
+  private Date gmtCreate;
+  private Date gmtUpdate;
+
+  private List<StockOutOrder> subOrders = Collections.emptyList();
 }
